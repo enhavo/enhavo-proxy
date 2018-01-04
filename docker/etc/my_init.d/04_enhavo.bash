7363c0695c0f0249460e03e6e96dcbe1fb6ede96
Gerhard Seidel <gseidel.message@googlemail.com>
2018-01-04 04:32:46 +0100

    execute full composer before running enhavo

--- docker/etc/my_init.d/04_enhavo.bash
+++ docker/etc/my_init.d/04_enhavo.bash
@@ -7,6 +7,6 @@ fi
 
 cd /var/www
 mysql -u root -proot -h mysql -e 'CREATE DATABASE IF NOT EXISTS enhavo'
+composer install
 app/console doctrine:schema:update --force
-app/console assets:install --symlink
 chown www-data:www-data -R .
