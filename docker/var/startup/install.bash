66fb40ed745f3d3c5d0fed2d80f9b7cb6989e12c
Gerhard Seidel <gseidel.message@googlemail.com>
2019-08-20 22:15:17 +0200

    create varnish secret if not exists

--- docker/var/startup/install.bash
+++ docker/var/startup/install.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -f /etc/varnish/secret ]; then
+    /var/www/current/bin/console proxy:varnish:create:secret
+fi
+
 echo "Install mysql"
 /var/www/current/bin/console doctrine:database:create
 /var/www/current/bin/console doctrine:migration:migrate -n
